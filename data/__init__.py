3eee0471654ae74d84a067cd14a557f635daa571
wuxiangchao <709894549@qq.com>
2025-08-05 15:52:06 +0800

    修改requirements.txt

---
+++ data/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: sirope
+# @Contact: cfd.xiangchao.wu@gmail.com
+# @License: (c)copyright 2018-2028
+# @Create Date: 2025/8/5 15:28
+# @Last Modified by: sirope
+# @Last Modified time: 2025/8/5 15:28
+# @File : __init__.py.py
+# @Description:
+
